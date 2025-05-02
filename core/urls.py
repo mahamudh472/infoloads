fda0e4526c3ec9913889b06c6bf32b53d9122fab
mahamudh472 <mahadymahamudh472@gmail.com>
2025-05-02 21:30:34 +0600

    app details page design started

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "core"
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('apps/<str:slug>', views.app_details, name="app_details"),
+    path('download/<str:slug>', views.app_download, name='app_download')
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from downloads.models import App 
 # Create your views here.
+
 def index(request):
     apps = App.objects.all()
     context = {
         'apps': apps
     }
     return render(request, 'index.html', context)
+
+def app_details(request, slug):
+    return render(request, 'downloads/app_details.html')
+
+def app_download(request, slug):
+    pass
