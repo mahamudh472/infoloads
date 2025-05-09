8016518e96c558ba8b792add69ca880f0dc3cc08
mahamudh472 <mahadymahamudh472@gmail.com>
2025-05-09 21:13:54 +0600

    Home page design improve

--- downloads/admin.py
+++ downloads/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import App, Category, Review, Download
+from .models import App, Category, Review, Download, Platform
 # Register your models here.
 
 admin.site.register(App)
 admin.site.register(Category)
 admin.site.register(Review)
 admin.site.register(Download)
+admin.site.register(Platform)
