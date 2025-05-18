80f2de4d913fbd29bfa703e2323e768282e8b196
Mahmud Hasan <106822443+mahamudh472@users.noreply.github.com>
2025-05-18 19:32:50 +0600

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -10,3 +10,13 @@ urlpatterns = [
     path('search', views.search_results, name="search_results"),
     path('category_details/<str:category_name>/<int:platform>', views.category_details, name="category_details")
 ]
+
+from django.views.static import serve
+from django.urls import re_path
+import os
+urlpatterns += [
+    re_path(r'^monetag_[a-z0-9]+\.js$', serve, {
+        'document_root': os.path.join(BASE_DIR, 'static'),  # or wherever the file is
+    }),
+]
+
