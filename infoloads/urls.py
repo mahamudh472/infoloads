f898a640ffdacac0fa24e7990aad3bd45063d04c
mahamudh472 <mahadymahamudh472@gmail.com>
2025-08-02 17:11:47 +0600

    SEO issue fixed

--- infoloads/urls.py
+++ infoloads/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('author/', include('author.urls')),
     path('', include('core.urls')),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path('ckeditor5/', include('django_ckeditor_5.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
